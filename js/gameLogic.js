3f82c50cdee8dd9bb7b2d6906b05c29412034ca3
Brian Beallo <bbeallo12@yahoo.com>
2016-03-11 03:45:36 -0800

    Fair Share Turns

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,79 +1,128 @@
-var board,
-    game = new Chess(),
-    messageField = $('#message'),
-    turn = 'w',
-	turn_count = 0;
+var board;
+var game = new Chess();
+var messageFieldNumb = $('#messageNumb');
+var messageField = $('#message');
+var messageFields = [];
+var turns = ['w','b','b','w','b','w','w','b','b','w','w','b','w'];
+var turn_count = 0;
+
+	messageFields[0] = $('#message0');
+    messageFields[1] = $('#message1');
+    messageFields[2] = $('#message2');
+    messageFields[3] = $('#message3');
+    messageFields[4] = $('#message4');
+    messageFields[5] = $('#message5');
+    messageFields[6] = $('#message6');
+    messageFields[7] = $('#message7');
+    messageFields[8] = $('#message8');
+    messageFields[9] = $('#message9');
+    messageFields[10] = $('#message10');
+    messageFields[11] = $('#message11');
+    messageFields[12] = $('#message12');
 
 // Only move if it is their turn and game is not over.
 // Modified chessboardjs.com/examples#5000
 var onDragStart = function(source, piece, position, orientation) {
-  if (game.game_over() === true ||
-      (turn === 'w' && piece.search(/^b/) !== -1) ||
-      (turn === 'b' && piece.search(/^w/) !== -1)) {
-    return false;
-  }
+    if (game.game_over() === true ||
+        (turns[0] === 'w' && piece.search(/^b/) !== -1) ||
+        (turns[0] === 'b' && piece.search(/^w/) !== -1)) {
+        return false;
+    }
 };
 
 // Move if legal
 // Modified chessboardjs.com/examples#5000
 var onDrop = function(source, target) {
-  var move = game.move({
-    from: source,
-    to: target,
-    promotion: 'q' // TODO Make promotion option
-  });
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' // TODO Make promotion option
+    });
 
-  if (move === null) {
-    return 'snapback';
-  }
+    if (move === null) {
+        return 'snapback';
+    }
 
-  // Valid move
-  // TODO Update to use fair sequence
-  turn = (turn === 'w' ? 'b' : 'w');
-  updateStatus();
+    // Valid move
+    turn_count = turn_count + 1;
+    for (i = 0; i <= 12; i++) {
+        turns[i] = (bitSum(turn_count + i) === 0 ? 'w' : 'b');
+    }
+    updateStatus();
+	return 0;
+};
+
+
+var bitSum = function(i) {
+    var sum = 0;
+    while (i > 0) {
+        sum = sum + i % 2;
+        i = Math.floor(i / 2);
+    }
+    return sum%2;
 };
 
 // Update the board after piece is done snapping
 // chessboardjs.com/examples#5000
 var onSnapEnd = function() {
-  board.position(game.fen());
+    board.position(game.fen());
 };
 
 // Update the turn
 // Modified chessboardjs.com/examples#5000
 var updateStatus = function() {
-  var message = '';
-  var moveColor = (turn === 'b' ? 'Black' : 'White');
 
-  if (game.in_checkmate() === true) {
-    message = 'Game over: ' + moveColor + ' is checkmated.';
-  }
-  else if (game.in_draw() === true) {
-    message = 'Game over: Draw';
-  }
-  else {
-    message = moveColor + ' to move';
+    var messageNumb = turn_count.toString();
+    var message = '';
+	var moveColor = "White";
+    var messages = [];
+    var moveColors = [];
 
-    if (game.in_check() === true) {
-      message += ', ' + moveColor + ' is in check';
+
+    for (i = 0; i <= 12; i++) {
+        messages[i] = '';
+        moveColors[i] = (turns[i] === 'b' ? 'Black' : 'White');
     }
-  }
 
-  messageField.html(message);
+
+    if (game.in_checkmate() === true) {
+        message = 'Game over: ' + moveColors[0] + ' is checkmated.';
+    } else if (game.in_draw() === true) {
+        message = 'Game over: Draw';
+    } else {
+        message = 'No Check';
+		
+        for (i = 0; i <= 12; i++) {
+            messages[i] = moveColors[i] + ' to move';
+        }
+
+        if (game.in_check() === true) {
+            message += ', ' + moveColors[0] + ' is in check';
+        }
+    }
+
+    messageFieldNumb.html(messageNumb);
+    messageField.html(message);
+    for (i = 0; i <= 12; i++) {
+        messageFields[i].html(messages[i]);
+    }
 };
 
 var init = function() {
-  var boardConfig = {
-    draggable: true,
-    dropOffBoard: 'snapback',
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onSnapEnd: onSnapEnd,
-    position: 'start'
-  };
 
-  board = ChessBoard('board', boardConfig);
-  updateStatus();
+    
+
+    var boardConfig = {
+        draggable: true,
+        dropOffBoard: 'snapback',
+        onDragStart: onDragStart,
+        onDrop: onDrop,
+        onSnapEnd: onSnapEnd,
+        position: 'start'
+    };
+
+    board = ChessBoard('board', boardConfig);
+    updateStatus();
 };
 
 $(document).ready(init);
